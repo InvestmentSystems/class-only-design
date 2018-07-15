a9bb0df51dd68fc5dce4cd0f2785104c17928e8d
tomrutherford <tmrutherford@gmail.com>
2018-07-15 01:38:44 -0700

    Add python 3.5 in setup.py, bump version

--- class_only_design/__init__.py
+++ class_only_design/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Tom Rutherford"""
 __email__ = "foreverwintr@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from class_only_design.core import class_only
 from class_only_design.core import constant
