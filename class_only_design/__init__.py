505ef66409828a490a2b4685ee982ffd11b10ab9
tomrutherford <tmrutherford@gmail.com>
2018-07-18 22:28:29 -0700

    Update version

--- class_only_design/__init__.py
+++ class_only_design/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Tom Rutherford"""
 __email__ = "foreverwintr@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from class_only_design.core import class_only
 from class_only_design.core import constant
